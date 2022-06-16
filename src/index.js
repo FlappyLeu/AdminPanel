e71fcabc112dfb9914b5ca93a094eb0baae06253
mstars_lab_04 <mstars_lab_04@mstars-lab-04s-iMac.local>
2022-06-16 10:17:54 +0800

    basically started

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./contexts/UserContext";
+import { OrderProvider } from "./contexts/OrderContext";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <OrderProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </OrderProvider>
+    </UserProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
